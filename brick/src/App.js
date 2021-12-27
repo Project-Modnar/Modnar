27747c0c3922e32e6e4e40a181fd4ae68fc398cc
minjekang <kangminje777@gmail.com>
2021-12-27 17:29:02 +0900

    Footer

--- brick/src/App.js
+++ brick/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
 import Profile from "./Profile";
 import Upload from "./Upload";
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Profile />
       <Upload />
+      <Footer />
     </>
   );
 }
